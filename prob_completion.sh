0bab9802a94f8aee2c9a3b3b96c6ba99dc16868a
David Schneider <david.schneider@bivab.de>
2013-01-21 17:12:59 +0100

    add scompletion support for -p suboptions

--- prob_completion.sh
+++ prob_completion.sh
@@ -1,23 +1,44 @@
+_probcli_pref_opts()
+{
+    opts=$(cat "${cache}" \
+        | grep '^\s\{1,\}[A-Z]\w*\s\{1,\}:' \
+        | awk '{print $1}')
+}
+_probcli_common_opts()
+{
+    opts=$(cat "${cache}" \
+        | grep '^\s\{1,\}\-\w' \
+        | awk '{ print $1 }')
+}
 _probcli()
 {
-    cmd=${COMP_WORDS[0]}
-    cur=${COMP_WORDS[COMP_CWORD]}
+    local cmd=${COMP_WORDS[0]}
+    local cur=${COMP_WORDS[${COMP_CWORD}]}
+    local prev=${COMP_WORDS[$((${COMP_CWORD}-1))]}
+
+    eval cmd=$cmd
     cache="/tmp/$(basename "${cmd}")_cache.txt"
-    if [ ! -f "${cache}" ]
-    then
-      ${cmd} -h >> "${cache}"
+    # fill cache
+    if [ ! -f "${cache}" ]; then
+      ${cmd} -h -v >> "${cache}"
     fi
-    opts=$(cat "${cache}" | grep '^\s\{1,\}\-\w' | awk '{ print $1 }')
+    # escape leading "-"
+    if [ "-" = "${cur:0:1}" ]; then
+      cur="\\"${cur}
+    fi
+
+    if [ "${prev}" = "-p" ]; then
+      _probcli_pref_opts
+    else
+      _probcli_common_opts
+    fi
+
     # filter options if we have a current prefix
     if [ $cur ]; then
-      # escape leading "-" 
-      if [ "-" = "${cur:0:1}" ]; then
-        cur="\\"${cur}
-      fi
       opts=$( for i in ${opts[@]} ; do echo $i ; done | grep "^${cur}" )
     fi
+
     COMPREPLY=( $opts )
 }
 complete -o bashdefault -o default -o nospace -F _probcli probcli.sh
 complete -o bashdefault -o default -o nospace -F _probcli probcli
-
