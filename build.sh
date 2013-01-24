5c542d1bb4291a30485955cdb66f56127160fc00
David Schneider <david.schneider@bivab.de>
2013-01-24 09:48:25 +0100

    script to generate completion information for probcli.
    Expects the path to the corresponding binary to be passed in the PROBCLI variable

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+if [ -z "$PROBCLI" ]; then
+	echo "PROBCLI needs to be set"
+	exit
+fi
+${PROBCLI} -h -v > cache.txt
+
+pref_opts=$(cat cache.txt \
+			| grep '^\s\{1,\}[A-Z]\w*\s\{1,\}:' \
+			| awk '{print $1}')
+
+opts=$(cat cache.txt \
+		| grep '^\s\{1,\}\-\w' \
+		| awk '{ print $1 }')
+rm cache.txt &> /dev/null
+
+opts=${opts//$'\n'/' '}
+pref_opts=${pref_opts//$'\n'/' '}
+
+sed -e "s/%{opts}/\"${opts}\"/g" \
+		-e "s/%{pref_opts}/\"${pref_opts}\"/g" \
+		completion.template > prob_completion.sh
